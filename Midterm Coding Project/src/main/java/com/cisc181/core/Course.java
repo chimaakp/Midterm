95d05184544595fac9728e45dfbb2892d14ef09b
chimaakp <chimaakp@ChimaII>
2016-03-20 23:36:30 -0400

    starting off. due tommorow

---
+++ Midterm Coding Project/src/main/java/com/cisc181/core/Course.java
@@ -0,0 +1,11 @@
+package com.cisc181.core;
+
+public class Course {
+	
+	public int GradePoints;
+	
+	public String CourseName;
+	
+	UUID CourseID;
+
+}
---
+++ Midterm Coding Project/src/main/java/com/cisc181/core/Semester.java
@@ -0,0 +1,8 @@
+package com.cisc181.core;
+import java.util.Date;
+
+public class Semester {
+	public Date StartDate;
+	public Date EndDate;
+
+}
